b7398165d79d07adf425cb779c0b9b00e8226730
sofiacottone <cottonesofia@outlook.it>
2024-03-20 16:45:50 +0100

    fix: minor changes

--- js/script.js
+++ js/script.js
@@ -28,22 +28,22 @@ const teams = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.png',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'walter-gordon-office-manager.jpg'
+        image: 'angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.png',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.png',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
         name: 'Scott Estrada',
